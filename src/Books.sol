e057170629a66dfd9ac7adf38897b7a57ed120ec
Deepak-Kumar-Baria <dbaria@my.bridgeport.edu>
2025-03-24 19:57:03 -0400

    Format Solidity files

--- src/Books.sol
+++ src/Books.sol
@@ -3,27 +3,28 @@ pragma solidity 0.8.13;
 
 contract Books {
     address owner;
-    
+
     struct Book {
         string title;
         string author;
         uint256 pages;
     }
-    
+
     Book public book;
-    
-    modifier onlyOwner {
+
+    modifier onlyOwner() {
+        // Add parentheses to the modifier
         require(msg.sender == owner, "You are not the Owner");
         _;
     }
-    
+
     constructor(string memory _title, string memory _author, uint256 _pages) {
         book.title = _title;
         book.author = _author;
         book.pages = _pages;
         owner = msg.sender;
     }
-    
+
     function update_pages(uint256 _pages) public onlyOwner {
         book.pages = _pages;
     }
