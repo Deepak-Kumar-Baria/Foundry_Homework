4dbdec6255cf2639e856daecc403f3214d8f2efb
Deepak-Kumar-Baria <dbaria@my.bridgeport.edu>
2025-03-24 19:48:02 -0400

    Initial commit - Foundry Homework

--- script/Books.s.sol
+++ script/Books.s.sol
@@ -1,16 +1,12 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.13;
+pragma solidity ^0.8.13;
 
-import "forge-std/Script.sol";
-import "../src/Books.sol";
+import {Books} from "../src/Books.sol";
 
-contract DeployBooks is Script {
-    function run() external {
-        vm.startBroadcast();
-        
-        // Deploy the Books contract with specified values
-        Books books = new Books("Programming Foundry", "Deepak Kumar Baria", 100);
-        
-        vm.stopBroadcast();
+contract DeployBooks {
+    Books public books;
+
+    function run() public {
+        books = new Books("Programming Foundry", "Deepak Kumar Baria", 100);
     }
 }
--- test/Books.t.sol
+++ test/Books.t.sol
@@ -1,16 +1,18 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.13;
+pragma solidity ^0.8.13;
 
-import "forge-std/Test.sol";
-import "../src/Books.sol";
+import {Test} from "forge-std/Test.sol";
+import {Books} from "../src/Books.sol";
 
 contract BooksTest is Test {
-    Books books;
+    Books public books;
 
+    // setUp function to deploy the contract
     function setUp() public {
         books = new Books("Programming Foundry", "Deepak Kumar Baria", 100);
     }
 
+    // Test: get_book should return the initial values
     function test_get_book() public {
         Books.Book memory book = books.get_book();
         assertEq(book.title, "Programming Foundry");
@@ -18,9 +20,10 @@ contract BooksTest is Test {
         assertEq(book.pages, 100);
     }
 
+    // Test: update_pages should change the number of pages
     function test_updatePages() public {
-        books.update_pages(200);
+        books.update_pages(150);
         Books.Book memory book = books.get_book();
-        assertEq(book.pages, 200);
+        assertEq(book.pages, 150);
     }
 }
